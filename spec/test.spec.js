3ae343a7a33ed09bcd405843312130c98d901329
Toh Poh Seng <14SIC072J@SIT.SingaporeTech.edu.sg>
2016-09-30 10:07:14 +0800

    Test Travis.CI

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -16,7 +16,7 @@ describe("Hello World Server", function() {
 
     it("returns Hello World", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Hello world");
+        expect(body).toBe("Hello world!");
         done();
       });
     });
